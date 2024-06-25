28e9ddc919a311fd4988751241063fe9f62f5aec
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-25 20:07:34 +0200

    add:client/App.tsx: Navbar has been added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,15 @@ import { createTheme } from '@mui/material/styles';
 
 import { themeSettings } from '@/theme';
 
+import Navbar from '@/scenes/navbar';
+
 function App() {
    const theme = useMemo(() => createTheme(themeSettings), []);
 
    return (
       <div className="app">
          <ThemeProvider theme={theme}>
+            <Navbar />
             <CssBaseline />
          </ThemeProvider>
       </div>
