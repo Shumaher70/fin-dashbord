d7e11fee631c256fa0b674a09ac46d1ce62f6214
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-26 13:42:29 +0200

    add:client/App.tsx: The dashboard has been added to the route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
@@ -7,25 +7,28 @@ import { createTheme } from '@mui/material/styles';
 import { themeSettings } from '@/theme';
 
 import Navbar from '@/scenes/navbar';
+import Dashboard from '@/scenes/dashboard';
 
 function App() {
    const theme = useMemo(() => createTheme(themeSettings), []);
 
    return (
       <div className="app">
-         <ThemeProvider theme={theme}>
-            <Navbar />
-            <CssBaseline />
-            <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-               <Routes>
-                  <Route path="/" element={<div>dashboard page0</div>} />
-                  <Route
-                     path="/predictions"
-                     element={<div>dashboard page0</div>}
-                  />
-               </Routes>
-            </Box>
-         </ThemeProvider>
+         <BrowserRouter>
+            <ThemeProvider theme={theme}>
+               <CssBaseline />
+               <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+                  <Navbar />
+                  <Routes>
+                     <Route path="/" element={<Dashboard />} />
+                     <Route
+                        path="/predictions"
+                        element={<div>dashboard page0</div>}
+                     />
+                  </Routes>
+               </Box>
+            </ThemeProvider>
+         </BrowserRouter>
       </div>
    );
 }
