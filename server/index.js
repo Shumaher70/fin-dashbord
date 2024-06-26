6d087a3b0b49c1d380a43fcaeaa55e135c5553cf
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-26 20:23:55 +0200

    add:server/index.js: the logic for moving dummy db to cloud db has  been added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import kpiRoutes from './routes/kpi.js';
+import KPI from './models/KPI.js';
+import { kpis } from './data/data.js';
+
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -16,10 +20,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use('/kpi', kpiRoutes);
+
 const PORT = process.env.PORT || 9000;
 mongoose
    .connect(process.env.MONGO_URL)
    .then(async () => {
       app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+      // ADD DATA ONE TIME ONLY OR AS NEEDED
+      // await mongoose.connection.db.dropDatabase();
+      // KPI.insertMany(kpis);
    })
    .catch((error) => console.log(`${error} did not connect`));
