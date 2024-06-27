3ae2ebc805d41e8c2ba260c10927503e4e9e8ed1
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-27 17:07:43 +0200

    add:client/api/ts: Additional getProducts has been added

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,16 +1,21 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { GetKpisResponse } from '@/state/types';
+
+import { GetKpisResponse, GetProductsResponse } from '@/state/types';
 
 export const api = createApi({
    baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
    reducerPath: 'main',
-   tagTypes: ['Kpis'],
+   tagTypes: ['Kpis', 'Products'],
    endpoints: (build) => ({
       getKpis: build.query<Array<GetKpisResponse>, void>({
          query: () => 'kpi/kpis/',
          providesTags: ['Kpis'],
       }),
+      getProducts: build.query<Array<GetProductsResponse>, void>({
+         query: () => 'product/products/',
+         providesTags: ['Products'],
+      }),
    }),
 });
 
-export const { useGetKpisQuery } = api;
+export const { useGetKpisQuery, useGetProductsQuery } = api;
