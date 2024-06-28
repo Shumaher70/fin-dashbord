1faae0dda99a0c1dc55195f69f257651501d4729
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-28 12:29:30 +0200

    add:server/index.js: Additional middleware for the Transaction has been added

--- server/index.js
+++ server/index.js
@@ -8,9 +8,12 @@ import morgan from 'morgan';
 
 import kpiRoutes from './routes/kpi.js';
 import productRoutes from './routes/product.js';
+import transactionRoutesRoutes from './routes/transaction.js';
+
+import Transaction from './models/Transaction.js';
 import Product from './models/Product.js';
 import KPI from './models/KPI.js';
-import { kpis, products } from './data/data.js';
+import { kpis, products, transactions } from './data/data.js';
 
 dotenv.config();
 const app = express();
@@ -24,6 +27,7 @@ app.use(cors());
 
 app.use('/kpi', kpiRoutes);
 app.use('/product', productRoutes);
+app.use('/transaction', transactionRoutesRoutes);
 
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -32,6 +36,8 @@ mongoose
       app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
       // ADD DATA ONE TIME ONLY OR AS NEEDED
       // await mongoose.connection.db.dropDatabase();
+      // KPI.insertMany(kpis);
       // Product.insertMany(products);
+      // Transaction.insertMany(transactions);
    })
    .catch((error) => console.log(`${error} did not connect`));
