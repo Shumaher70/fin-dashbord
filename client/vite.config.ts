ba66c14d374fb9b26f43a7ba86fdd4026f5fef98
Oleg Vrabiy <olegvraby@gmail.com>
2024-07-01 15:00:46 +0200

    fix: client/vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
    resolve: {
       alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+   },
+   define: {
+      'process.env': process.env,
    },
 });
